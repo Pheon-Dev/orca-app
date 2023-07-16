b9f77edd1707e15679f40efdc48da728cc4aa111
Pheon-Dev <devpheon@gmail.com>
2023-07-16 11:47:09 +0300

    [+] index.html [+] App component

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use leptos::*;
+
+#[component]
+fn App(cx: Scope) -> impl IntoView {
+    let (count, set_count) = create_signal(cx, 0);
+
+    view! { cx, 
+        <button on:click=move |_| {
+            set_count(3);
+        }>
+        "Click Me: "
+            {move || count.get()}
+        </button>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    mount_to_body(|cx| view! { cx, <App/> })
 }
