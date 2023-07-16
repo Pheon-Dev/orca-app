3779d67c1788858d804d382161304b8b99e82865
Pheon-Dev <devpheon@gmail.com>
2023-07-16 16:41:46 +0300

    [+] logging

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
 use leptos::*;
-
-#[component]
-fn App(cx: Scope) -> impl IntoView {
-    let (count, set_count) = create_signal(cx, 0);
-
-    view! { cx, 
-        <button on:click=move |_| {
-            set_count(3);
-        }>
-        "Click Me: "
-            {move || count.get()}
-        </button>
-    }
-}
+pub use orca_app::*;
 
 fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
     mount_to_body(|cx| view! { cx, <App/> })
 }
